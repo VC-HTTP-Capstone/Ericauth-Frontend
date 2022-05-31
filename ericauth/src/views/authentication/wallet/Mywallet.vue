0a8bce80d467e992041f01b2cd7878b0b1ef1455
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-06-01 01:53:29 +0900

    merge files

--- ericauth/src/views/authentication/wallet/Mywallet.vue
+++ ericauth/src/views/authentication/wallet/Mywallet.vue
@@ -1,47 +1,76 @@
 <template>
 <div>
   <LeftSideBar></LeftSideBar>
-  <div>
-    <div>
+  <div class="parent">
+    <div class="child2">
     </div>
-    <div style="height:200px;line-height:200px;">
-      <center>
-        <span style="font-size:50px;">인증서를 골라주세요</span>
-      </center>
-    </div>
-    <div v-show="toggle">
+    <div class="child1" v-show="toggle">
       <div>
         <center>
-          {{ this.Qrname }}
-          <wj-barcode-qr-code :value="theValue"></wj-barcode-qr-code>
-          <button @click="change_toggle"> 확인 </button>
-          <button @click="Delete"> 삭제 </button>
-        </center>
-      </div>
-    </div>
-    <div v-show="!toggle">
-      <div v-for="(row, index) in rows" :key="row.id">
-        <center>
-          <div class="divbox">
-            <div style="height:100px;">
-                <button class="btnbox" @click="check(index)">
-                  <div>
-                    <img
-                      class="qr"
-                      style="background-color:#AFC4E7;"
-                      src="../../../assets/qr.png"
-                    />
-                    <span class="wordbox">
-                      {{ row }}
-                    </span>
-                  </div>
-                </button>
+          <div style="border:solid 1px; height: 15vh;line-height:700%; 
+          background-color:#687C99;">
+            <span style="font-size:40px; color:white;">{{ this.Qrname }}</span>
+          </div>
+          <div style="height: 60vh;margin-top:10%;">
+            <div>
+              <wj-barcode-qr-code style="height:60vh; width:60vh;" :value="theValue" ></wj-barcode-qr-code>
             </div>
+          </div>
+          <div style="margin-top:10%;">
+            <button class="btnboxcheck" @click="change_toggle"> 
+              <span style="color:white;">확인</span> 
+            </button>
+            <button class="btnboxrm" @click="Delete">
+              <span style="color:white;">삭제</span>
+            </button>
           </div>
         </center>
       </div>
     </div>
-    <div>
+    <div style="background-color:#687C99;" class="child1" v-show="!toggle">
+      <div style="border:solid 1px;height:13%; line-height:650%; background-color:#687C99;">
+        <center>
+          <span style="font-size:30px;color:white;"> 인증서를 선택해 주세요 </span>
+        </center>
+      </div>
+      <div style="height:20%; border:solid 1px; background-color:white;" v-for="(row, index) in rows" :key="row.id">
+        <div class="divbox">
+            <div class="btnparent">
+              <div class="btnchild1"> 
+                <img class="qr"
+                  src="../../../assets/qr.png"
+                />
+                
+              </div>
+              <div class="btnchild2">  
+                <div>
+                  <span class="wordbox">
+                    이름 : {{ row }}
+                  </span>
+                </div>
+                <div>
+                  <span class="wordbox">
+                    발급 날짜 : 2022/05/07
+                  </span>
+                </div>
+                <div>
+                  <span class="wordbox">
+                    만료 날짜 : 2023/05/07
+                  </span>
+                </div>
+              </div>
+              <div style="margin-right: 2%;" class="btnchild1"> 
+                <button class="btnbox" @click="check(index)">
+                  <span style="font-size:30px; color:white;"> view </span>
+                </button>
+                
+              </div>
+            </div>
+        </div>
+        
+      </div>
+    </div>
+    <div class="child2">
     </div>
   </div>
 
@@ -147,37 +176,75 @@ export default {
 
 <style scoped>
 .divbox {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  width : 40%;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  border : solid 1px;
+  width : 100%;
+  border: solid 1px;
+  height: 100%;
 }
 .btnbox {
   width:100%;
   height: 100%;
+  background-color: #40AFFF;
+  border: 0;
+  outline: 0;
+  padding:0;
+}
+.btnbox:hover {
+  background-color:#399EE5;
+}
+.btnboxrm {
+  border: 0;
+  outline: 0;
+  padding:0;
+  width:20vh;
+  height: 5vh;
+  background-color: #fd5e53;
+  margin-left:5%;
+}
+.btnboxrm:hover {
+  background-color: #D11507;
+}
+.btnboxcheck {
+  border: 0;
+  outline: 0;
+  padding:0;
+  width:20vh;
+  height: 5vh;
+  background-color: #21bf73;
+  margin-right:5%;
+}
+.btnboxcheck:hover {
+  background-color: #4aa96c;
 }
 .qr {
-  height: 90px;
-  width: 15%;
-  float: left;
+  height: 15vh;
+  width: 15vh;
 }
 .wordbox {
-  font-size: 30px;
-  line-height: 100px;
+  font-size: 20px;
 }
 .parent {
   display: flex;
   height: 100vh;
-  position: relative;
 }
 .child1 {
   flex: 3;
 }
 .child2 {
-  flex: 1;
+  flex: 2;
+  background-color:#F2FBFD;
 }
+.btnparent {
+  display: flex;
+}
+.btnchild1 {
+  flex: 1;
+  margin-top:3%;
+  margin-left:1%;
+}
+.btnchild2 {
+  flex: 2;
+  margin-top:6%;
+}
+
 
 </style>
